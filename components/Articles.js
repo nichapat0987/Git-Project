ccdb9af1196b5b7b9f4c83ea6454f959a2e4cda4
Nichapat Nitikhunkasem <nichapatn@MacBook-Air-khxng-NichapatN.local>
2021-05-13 19:34:49 +0700

    login with redux

--- components/Articles.js
+++ components/Articles.js
@@ -1,15 +1,27 @@
 import React, {Component} from 'react'
 import {View, Text, StyleSheet, TextInput,TouchableOpacity, Button} from 'react-native'
 import firebase from 'firebase'
+import PostForm from './PostForm'
+import AllPosts from './AllPosts'
+import postReducer from '../reducers/postReducers'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 
+const store = createStore(postReducer);
 
 class Articles extends Component {
 
     render(){
         return (
         <View style={styles.container}>
+            <Provider store={store}>
+               <PostForm/>
+               <AllPosts />
+            </Provider>
+            <TouchableOpacity onPress={() => firebase.auth().signOut()}>
+            <Text style={styles.LogoutButton}>Logout</Text>
+            </TouchableOpacity>
 
-            <Text>This is for fill in.</Text>
 
         </View>
         )
@@ -22,6 +34,9 @@ const styles= StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center'
     },
+    LogoutButton: {
+        color:'red'
+    }
 })
 
 export default Articles;
---
+++ components/PostForm.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    Button
+} from "react-native";
+import {connect} from 'react-redux'
+
+class PostForm extends Component {
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const data = {
+            id: new Date(),
+            firstname: this.state.firstname,
+            lastname: this.state.lastname
+        }
+        
+        console.log("pressed!")
+        console.log(data)
+       
+        this.props.dispatch({
+            type: 'ADD_POST',
+            data
+        })
+
+        this.state.firstname="";
+        this.state.lastname="";
+    }
+    render(){
+        return(
+            <View styles={styles.container} onPress={this.handleSubmit}>
+                <Text style={styles.Header}>Fill In</Text>
+                
+                <TextInput
+                    placeholder="Firstname"
+                    onChangeText={(firstname) => this.setState({firstname})}
+                />
+
+                <TextInput
+                    placeholder="Lastname"
+                    onChangeText={(lastname) => this.setState({lastname})}
+                />
+
+                <Button title='SUBMIT' onPress={this.handleSubmit}/>
+                
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    Header: {
+        fontSize: 20
+    }
+})
+
+export default connect()(PostForm);
